40673245fa2ff474836f2d0700172bec24960604
Mateus Quaresma <mateus.quaresma@protonmail.com>
2017-07-29 12:23:43 -0300

    Finished lesson 20

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,9 +7,22 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {term : ""};
+  }
+
   // default method to render a react component
   render() {
-    return <input onChange = {event => console.log(event.target.value)} />
+    return (
+      <div>
+        <input
+          value = {this.state.term}
+          onChange = {event => this.setState({term: event.target.value})} />
+      
+      </div>
+    );
   }
 
 
